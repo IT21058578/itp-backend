d948244bf8869b2416714fdc3a5a8b82a32a638d
TharinduSampath <gunasekeratharindu@gmail.com>
2022-09-05 14:24:43 +0530

    Created custom heap data structures

--- src/main/java/com/web/backend/utils/MinHeap.java
+++
@@ -1,4 +0,0 @@
-package com.web.backend.utils;
-
-public class MinHeap {
-}
---
+++ src/main/java/com/web/backend/utils/heaps/CustomMaxHeap.java
@@ -0,0 +1,33 @@
+package com.web.backend.utils.heaps;
+
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.PriorityQueue;
+
+/**
+ * Heap that stores the smallest values passed into it in a largest-first order.
+ * Takes in key-value pairs. Compares key to maintain order.
+ * @param <T> Type of the value passed in.
+ */
+public class CustomMaxHeap<T> {
+    int maxSize;
+    PriorityQueue<HeapNode<T>> queue;
+
+    public CustomMaxHeap(int maxSize) {
+        this.maxSize = maxSize;
+        this.queue = new PriorityQueue<>(maxSize, Comparator.reverseOrder());
+    }
+
+    public void add(double key, T value) {
+        if (queue.size() == maxSize && queue.peek().getKey() > key) {
+            queue.poll();
+            queue.add(new HeapNode<>(key, value));
+        } else if (queue.size() < maxSize) {
+            queue.add(new HeapNode<>(key, value));
+        }
+    }
+
+    public T pollAndGetValue() {
+        return Objects.requireNonNull(queue.poll()).getValue();
+    }
+}
