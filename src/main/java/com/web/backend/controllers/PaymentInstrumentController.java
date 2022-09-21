84997ce0614bea80d4b2f51d52cc643eecb6ae5f
Gayan Kalinga Bulathsinhala <bg15407@gmail.com>
2022-09-21 20:27:35 +0530

    adding payment instrument controller

--- src/main/java/com/web/backend/controllers/PaymentInstrumentController.java
+++ src/main/java/com/web/backend/controllers/PaymentInstrumentController.java
@@ -1,4 +1,10 @@
 package com.web.backend.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/instruments")
 public class PaymentInstrumentController {
+
 }
