1d1b6664b008e87f6ba4c66ecd8efa1b4d4e48a1
Gayan Kalinga Bulathsinhala <bg15407@gmail.com>
2022-09-24 21:20:46 +0530

    add abitlity to call the API from local host

--- src/main/java/com/web/backend/webcontroller/InvoiceWebServiceController.java
+++ src/main/java/com/web/backend/webcontroller/InvoiceWebServiceController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api")
 public class InvoiceWebServiceController {
 
