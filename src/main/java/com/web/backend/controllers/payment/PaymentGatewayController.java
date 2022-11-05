b3352e9955164a2018af6d7565a6d7353c0b7a60
Gayan Kalinga Bulathsinhala <bg15407@gmail.com>
2022-11-05 21:49:59 +0530

    Added the Payment gateway controller & Stripe Client

---
+++ src/main/java/com/web/backend/controllers/payment/PaymentGatewayController.java
@@ -0,0 +1,20 @@
+package com.web.backend.controllers.payment;
+import com.stripe.model.Charge;
+import com.web.backend.services.payment.StripeClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/payment")
+public class PaymentGatewayController {
+
+    private StripeClient stripeClient;
+    @Autowired
+    PaymentGatewayController(StripeClient stripeClient) {
+        this.stripeClient = stripeClient;
+    }
+    @PostMapping("/charge")
+    public Charge chargeCard(@RequestHeader(value="token") String token, @RequestHeader(value="amount") Double amount) throws Exception {
+        return this.stripeClient.chargeNewCard(token, amount);
+    }
+}
