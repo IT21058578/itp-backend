3ef6a3c097f1e066e3fa7e9b18ca9246fd247c7f
Gayan Kalinga Bulathsinhala <bg15407@gmail.com>
2022-11-06 09:12:14 +0530

    few modification to invoice update

--- src/main/java/com/web/backend/controllers/payment/PaymentGatewayController.java
+++ src/main/java/com/web/backend/controllers/payment/PaymentGatewayController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/api/payment")
+@RequestMapping("/api/v1/payment")
 public class PaymentGatewayController {
 
     private StripeClient stripeClient;
